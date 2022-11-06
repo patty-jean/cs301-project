f8640a525ad2bc6d4783c35a2b7772b8f58afaca
patty-jean <84546784+patty-jean@users.noreply.github.com>
2022-11-06 16:45:00 -0500

    Update 228_training_aerial_imagery.py

--- 228_training_aerial_imagery.py
+++ 228_training_aerial_imagery.py
@@ -375,26 +375,34 @@ print("Mean IoU =", IOU_keras.result().numpy())
 #Predict on a few images
 
 import random
-#test_img_number = random.randint(0, len(X_test))
-test_img_number = 10
-test_img = X_test[test_img_number]
-ground_truth=y_test_argmax[test_img_number]
-#test_img_norm=test_img[:,:,0][:,:,None]
-test_img_input=np.expand_dims(test_img, 0)
-prediction = (model.predict(test_img_input))
-predicted_img=np.argmax(prediction, axis=3)[0,:,:]
+
+nums=[]
+
+for i in range(10):
+    test_img_number = random.randint(0, len(X_test))
+    while test_img_number in nums:
+        test_img_number = random.randint(0, len(X_test))
+    nums.append(test_img_number)
+    #test_img_number = 10
+    test_img = X_test[test_img_number]
+    ground_truth=y_test_argmax[test_img_number]
+    #test_img_norm=test_img[:,:,0][:,:,None]
+    test_img_input=np.expand_dims(test_img, 0)
+    prediction = (model.predict(test_img_input))
+    predicted_img=np.argmax(prediction, axis=3)[0,:,:]
 
 
-plt.figure(figsize=(12, 8))
-plt.subplot(231)
-plt.title('Testing Image')
-plt.imshow(test_img)
-plt.subplot(232)
-plt.title('Testing Label')
-plt.imshow(ground_truth)
-plt.subplot(233)
-plt.title('Prediction on test image')
-plt.imshow(predicted_img)
-plt.savefig('output.png')
+    plt.figure(figsize=(12, 8))
+    plt.subplot(231)
+    plt.title('Testing Image')
+    plt.imshow(test_img)
+    plt.subplot(232)
+    plt.title('Testing Label')
+    plt.imshow(ground_truth)
+    plt.subplot(233)
+    plt.title('Prediction on test image')
+    plt.imshow(predicted_img)
+    output_num = "output" + str(i)
+    plt.savefig('output.png')
 
 #####################################################################
